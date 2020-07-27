d213b9d4abe00b530a145106799b4d1d3cec373c
Jeovanni <jeovannildg@gmail.com>
2020-07-26 21:41:47 -0600

    added map for comments

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -11,6 +11,9 @@ const CommentSection = props => {
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((data) => (
+        <Comment comment = {data}/>
+      ))}
       <CommentInput />
     </div>
   );
